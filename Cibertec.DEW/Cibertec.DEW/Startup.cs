0609e57cff651a908df445268261e7464f260782
MARC <m.rojascoraje@outlook.com>
2016-11-01 16:46:01 -0500

    Proyecto inicial

---
+++ Cibertec.DEW/Cibertec.DEW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cibertec.DEW.Startup))]
+namespace Cibertec.DEW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
